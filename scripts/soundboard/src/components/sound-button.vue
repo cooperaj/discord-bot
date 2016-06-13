bd852bad80658cdba0d47210f81cea08410a583e
Adam Cooper <adam@networkpie.co.uk>
2016-06-13 11:04:48 +0100

    Moved to using Vue components for the soundboard.
    Uses webpack to build them all using the command.
    
    $node ../../../node_modules/webpack/bin/webpack.js --cache false
    
    From within the scripts/soundboard directory.

---
+++ scripts/soundboard/src/components/sound-button.vue
@@ -0,0 +1,38 @@
+<template>
+<button name="sound"
+    class="btn btn-default"
+    id="{{ key }}"
+    value="{{ key }}"
+    v-on:click="playSound(key)">{{{ key }}}</button>
+</template>
+
+<script>
+
+export default {
+
+    props: [
+        'key',
+        'user'
+    ],
+
+    methods: {
+        playSound: function(identifier) {
+            var xhr = new XMLHttpRequest()
+            var self = this
+            xhr.open('GET', '/soundboard/api/sounds/' + encodeURIComponent(identifier) + '/' + user.user_id)
+            xhr.onload = function () {
+                // don't do anything just yet
+            }
+            xhr.send()
+        }
+    }
+}
+</script>
+
+<style>
+.btn {
+    margin: 0.4em;
+    border-radius: 0;
+    flex-grow: 1;
+}
+</style>
