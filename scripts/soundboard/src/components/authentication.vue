24e4f20a2681ab7b6e372b13c479e66361776c57
Adam Cooper <adam@networkpie.co.uk>
2017-08-10 10:16:00 +0100

    Grammar improvments

--- scripts/soundboard/src/components/authentication.vue
+++ scripts/soundboard/src/components/authentication.vue
@@ -2,7 +2,7 @@
 <div id="logindialog" class="row" v-if="!user">
     <div class="col-sm-8 col-sm-offset-2">
         <p>To access the soundboard you will need to login using your Discord credentials. Don't worry, we let Discord
-            do all the work and so won't see your precious.</p>
+            do all the work so won't see your precious.</p>
         <a href="/auth" class="btn btn-default" role="button">Login</a>
     </div>
 </div>
